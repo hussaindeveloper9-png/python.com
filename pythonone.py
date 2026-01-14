5a6253e97e1b5aa696bd209b7592ab1032e2d3d0
unknown <hussaindeveloper9@gmail.com>
2026-01-14 12:35:40 +0500

    first commit

---
+++ pythonone.py
@@ -0,0 +1,79 @@
+# web = [ "html , css , js ", "bootstrap"]
+# print(web)
+# web = ["headings", "paragraph" , "bold"]
+# print(type(web))
+# web = ["int","float" ,"string"]
+# print(web[0])
+# web = ["int","float" ,"string"]
+# web[0]= "boolean"
+# print(web)
+# web = ["int","float" ,"string"]
+# web[0:3]= "boolean"
+# print(web)
+# web = ["int","float" ,"string"]
+# web.append("boolean")
+# print(web)
+# web = ["int","float" ,"string"]
+# web.insert(2,"boolean")
+# print(web)
+# web = ["int","float" ,"string"]
+# web.extend("boolean")
+# print(web)
+# web = ["int","float" ,"string"]
+# web.remove("float")
+# print(web)
+# web = ["int","float" ,"string"]
+# web.pop(1)
+# print(web)
+# web = ["int","float" ,"string"]
+# web.clear()
+# print(web)
+# web = ["int","float" ,"string"]
+# print(len(web))
+# web = ["int","float" ,"string"]
+# for item in web :
+#   print(item)
+# numbers = [1,11,44,123,132,341,445]
+# empty = []
+# for x in numbers :
+#     if "1" in str(x):
+#         empty.append(x)
+# print(empty)
+# data = ["int","float" ,"string"]
+# empty = [x for x in data if "t" in x]
+# print(empty)
+# newlist = [x for x in range(10) if x % 2 == 0]
+# print(newlist)
+# newlist = [x for x in range(20) if x % 2 != 0]
+# print(newlist)
+# abc =['m','b','d','c','g','f','e','h','k','a','j','i','l']
+# abc.sort()
+# print(abc)
+# abc =[1, 34 , 45 , 78 , 23 , 0 , 9 , 67 , 100 , 4]
+# abc.sort()
+# print(abc)
+# web = ["int","float" ,"string"]
+# mylist = web.copy()
+# print(mylist)
+
+# web = ["int","float" ,"string"]
+# developer = ["x","y","z"]
+# list3 = web + developer
+# print(list3)
+# pythonlist
+# accesslist
+# removelist
+# updatelist
+# looplist
+# iflist
+# listmethodes
+# sortlist
+# copylist
+# -------------------------------------------------------------------------------
+# print ("tuple")
+
+# tuple = ("web" , "develooper " , "hussian" )
+# print(tuple)
+
+# tuple = (1 , 2  , 3 , 4 , 5  )
+# print(type(tuple)
